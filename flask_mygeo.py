1ef21c1b0965773a66267468c28df5cf5bac6329
MaEnKa <markusenke@gmx.net>
2017-08-02 13:59:34 +0200

    added static htmlpage with simple leaflet application

--- flask_mygeo.py
+++ flask_mygeo.py
@@ -9,7 +9,7 @@ sys.setdefaultencoding('utf-8')
 
 
 ## from helloworld example 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 #for testing enter the dbpath
 db_path = '/home/ma/my_bremen_osm_1.sqlite'
@@ -51,7 +51,7 @@ def show_db(lat, lon):
         )
         ''').format(lat, lon)) #%(lat,lon,buff_lat,buff_lon))
     db_entries = cursr.fetchall()
-    
+
     #convert to string
     return str(db_entries[0][0] +" "+db_entries[1][0])
 
@@ -71,9 +71,10 @@ def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
 
+#return static html page
 @app.route('/')
-def hello_world():
-    return 'Hello, World! Nice to meet you!'
+def root():
+    return app.send_static_file('index.html')
 
 
 #get the geocoordinates from url and make a db call 
